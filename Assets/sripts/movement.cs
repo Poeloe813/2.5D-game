7bff249989ecc73e50c2320d5a33ff8240a20eec
Poeloe <jmpul813@gmail.com>
2024-09-19 10:26:07 +0200

    initial commit

---
+++ Assets/sripts/movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement1 : MonoBehaviour
+{
+    public InputAction PlayerControls;
+    public float Xmove2;
+    public float Ymove2;
+    public float Zmove2;
+
+
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = g
+    }
+    void Update()
+    {
+
+    }
+}
