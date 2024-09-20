13336ee11d53a9b365b51483777b96a3ad336855
Poeloe <jmpul813@gmail.com>
2024-09-20 09:51:57 +0200

    movement script

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -1,28 +1,69 @@
+using System.ComponentModel.Design;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement1 : MonoBehaviour
 {
-    public InputAction PlayerControls;
-    public float Xmove2;
-    public float Ymove2;
-    public float Zmove2;
-    public Player Player;
+    public InputAction moveRight;
+    public InputAction moveLeft;
+    public InputAction moveUp;
+    public InputAction moveDown;
 
     private Rigidbody rb;
-    void Start()
+
+    public float SpeedLeftRight = 5f;
+    public float SpeedUp = 4f;
+    public float SpeedDown = -4f;
+    public float SpeedSink = 2f;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    void Update()
-    {
-        Xmove2 = PlayerControls.ReadValue<Vector3>().x;
-        Ymove2 = PlayerControls.ReadValue<Vector3>().y;
-        Zmove2 = PlayerControls.ReadValue<Vector3>().z;
-        PlayerControls.ReadValue<Vector3>();
 
-        Vector3 movement = new Vector3(Xmove2, Ymove2, Zmove2);
-        rb.velocity = transform.forward * Zmove2 * Player.Speed;
-        transform.Rotate(Vector3.up, Xmove2 * Player.BodyRotationSpeed * Time.deltaTime);
+    private void Update()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+
+        if (moveLeft.ReadValue<float>() == 1)
+        {
+            rb.velocity += new Vector3(-SpeedLeftRight, 0, 0);
+        }
+
+        else if (moveRight.ReadValue<float>() == 1)
+        {
+            rb.velocity += new Vector3(SpeedLeftRight, 0, 0);
+        } 
+
+
+
+        if (moveUp.ReadValue<float>() == 1)
+        {
+            rb.velocity += new Vector3(0, SpeedUp, 0);
+        }
+
+        else if (moveDown.ReadValue<float>() == 1)
+        {
+            rb.velocity += new Vector3(0, SpeedDown, 0);
+        }
+        else
+        {
+            rb.velocity += new Vector3(0, -SpeedSink, 0);
+        }
+    }
+
+    private void OnEnable()
+    {
+        moveRight.Enable();
+        moveLeft.Enable();
+        moveUp.Enable();
+        moveDown.Enable();
+    }
+    private void OnDisable()
+    {
+        moveRight.Disable();
+        moveLeft.Disable();
+        moveUp.Disable();
+        moveDown.Disable();
     }
 }
